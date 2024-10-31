7ea7ce4a1b80ab704b5bf500de94efc27fa98c0f
Dev <iwwwi2015@gmail.com>
2024-10-31 11:01:34 -0400

    v1.0.2

--- src/Routes/admin-routes.php
+++ src/Routes/admin-routes.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 Route::group([
-    'prefix' => config('app.admin_url') . '/integration-version',
-    'middleware' => ['web', 'admin']
+    'prefix'     => 'v1/admin',
+    'middleware' => ['sanctum.locale'],
 ], function () {
 
     Route::post('get-identities', [\IntegrationHelper\IntegrationVersionLaravelServer\Http\Controllers\Admin\IntegrationVersionController::class, 'getIdentities'])
